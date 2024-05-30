75a37f8699acc0ec09d82e3e4e108d8293c76df0
lsl <2773161651@qq.com>
2024-05-30 14:20:36 +0800

    第一次修改

--- src/HelloWorld.go
+++ src/HelloWorld.go
@@ -3,34 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!"+"123")
+	fmt.Println("Hello, World!" + "12345")
 	fmt.Println("菜鸟教程：runoob.com")
-	var stockcode =123
-	var enddate="2020-12-31"
-	var url="Code=%d&endDate=%s"
-	var target_url=fmt.Sprintf(url,stockcode,enddate)
+	var stockcode = 123
+	var enddate = "2020-12-31"
+	var url = "Code=%d&endDate=%s"
+	var target_url = fmt.Sprintf(url, stockcode, enddate)
 	fmt.Println(target_url)
 	//fmt.Printf(url,stockcode,enddate)
-	var a,b = 9,2
+	var a, b = 9, 2
 	//var c = true
 	var c = false
 	var d = "hello"
-	if c{
-	  fmt.Println(a,b,c,d)
+	if c {
+		fmt.Println(a, b, c, d)
 	}
-	for a <= 13{
-		fmt.Println("Hello, World!"+"123")
+	for a <= 13 {
+		fmt.Println("Hello, World!" + "123")
 		a++
 	}
-	fmt.Println(a,b)
-	ret := max1(a,b)
-	fmt.Printf( "最大值是 : %d\n", ret )
+	fmt.Println(a, b)
+	ret := max1(a, b)
+	fmt.Printf("最大值是 : %d\n", ret)
 	//a1, b1 := swap("Google", "Runoob")
 	var a1, b1 = swap("Google", "Runoob")
 	fmt.Println(a1, b1)
 
 	var n [10]int /* n 是一个长度为 10 的数组 */
-	var i,j int
+	var i, j int
 
 	/* 为数组 n 初始化元素 */
 	for i = 0; i < 10; i++ {
@@ -39,31 +39,32 @@ func main() {
 
 	/* 输出每个数组元素的值 */
 	for j = 0; j < 10; j++ {
-		fmt.Printf("Element[%d] = %d\n", j, n[j] )
+		fmt.Printf("Element[%d] = %d\n", j, n[j])
 	}
 
 	//var g = 20   /* 声明实际变量 */
 	var g int
 	g = 10
-	var ip *int        /* 声明指针变量 */
+	var ip *int /* 声明指针变量 */
 
-	ip = &g  /* 指针变量的存储地址 */
+	ip = &g /* 指针变量的存储地址 */
 
-	fmt.Printf("g 变量的地址是: %x\n", &g  )
+	fmt.Printf("g 变量的地址是: %x\n", &g)
 
 	/* 指针变量的存储地址 */
-	fmt.Printf("ip 变量储存的指针地址: %x\n", ip )
+	fmt.Printf("ip 变量储存的指针地址: %x\n", ip)
 
 	/* 使用指针访问值 */
-	fmt.Printf("*ip 变量的值: %d\n", *ip )
+	fmt.Printf("*ip 变量的值: %d\n", *ip)
 
 }
+
 /* 函数返回两个数的最大值 */
 func max1(num1, num2 int) int {
 	/* 定义局部变量 */
 	var result int
 
-	if (num1 > num2) {
+	if num1 > num2 {
 		result = num1
 	} else {
 		result = num2
